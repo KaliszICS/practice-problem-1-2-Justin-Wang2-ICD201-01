1d2812e41532ffc9a9e1259139b9c9e6ed92fb1b
Justin-Wang2-ICD201-01 <justin.wang@student.tdsb.on.ca>
2024-10-02 13:41:14 +0000

    ju

--- PP1_2.py
+++ PP1_2.py
@@ -3,19 +3,34 @@
 #Write Assignment code here
 
 def q1():
-
+    _name_ = ("Alex")
+    print(_name_)
 def q2():
-
+    num1 = 3
+    num2 = 2
+    num3 = 5
+    print(num1 + num2 + num3)
 def q3():
-
+    num1 = 21
+    num2 = 4
+    print(num1 // num2)
 def q4():
-
+    num1 = 6.3
+    num2 = 3.0
+    print(num1 / num2)
 def q5():
-
+    word = ("Hello World")
+    print(word)
+    word = ("Goodbye World")
+    print(word)
 def q6():
+    word1 = True
+    print(word1)
 
 def q7():
-
+    num1 = 7
+    num2 = 2
+    print(num1 % num2)
 q1()
 q2()
 q3()
@@ -23,3 +38,4 @@ q4()
 q5()
 q6()
 q7()
+
